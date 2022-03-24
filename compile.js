fa7b32f419a7792cfe05c8abc0517d9e2aa2ea8f
zelva3 <stselva.avles@gmail.com>
2022-03-24 23:04:58 +0530

    Initial Release
    Initial code

---
+++ compile.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+const lottery = {
+  language: 'Solidity',
+  sources: {
+    'Lottery.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(
+  solc.compile(JSON.stringify(lottery))).contracts[
+  'Lottery.sol'
+].Lottery;
